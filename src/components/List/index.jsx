e2e99fea75aec61e817465d2a64142e109927e90
Sergey Antonov <flyker777@yandex.ru>
2020-01-19 20:35:04 +0300

    add new component list

---
+++ src/components/List/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './List.scss';
+
+const List = ({ items }) => {
+  return (
+    <ul className="list">
+      {items.map((item, index)=> (
+        <li key={index}>
+          <i>
+            {item.icon ? ( item.icon ) : (
+              <i className={`badge badge--${item.color}`}></i>
+            )}
+          </i>
+          <span>{item.name}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
